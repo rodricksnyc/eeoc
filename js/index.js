8d8aba5df54aae53a415ae503d8c4a0443bf77e0
Elizabeth <elizabeth@Elizabeths-iMac.local>
2021-02-18 16:21:56 -0500

    ERGgwe

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
 //508 tabbing
 
-$("a, button, input, [tabIndex='0'], #one").on("keyup", function (e) {
+$("a, button, input, [tabIndex='0'], #one, .closeRadio").on("keyup", function (e) {
   var code = (e.keyCode ? e.keyCode : e.which);
   if (code == 9) {
     $(this).css('outline', 'dashed 3px #4599ff')
   }
 
 })
-$("a, button, input, [tabIndex='0'], #one").on('focusout', function() {
+$("a, button, input, [tabIndex='0'], #one, .closeRadio").on('focusout', function() {
   $(this).css('outline', 'none')
 })
 
+
+$(".backButton").on("keyup", function (e) {
+  var code = (e.keyCode ? e.keyCode : e.which);
+  if (code == 9) {
+    $(this).find('.circle').css('outline', 'dashed 3px #4599ff')
+  }
+
+})
+
+$(".backButton").on('focusout', function() {
+  $(this).find('.circle').css('outline', 'none')
+})
 
 
 //rotate caret
@@ -284,19 +296,19 @@ $('#close').keypress(
   close
 );
 
-$('.form-check input').on('keyup', function(e) {
+$('#slideOut .form-check input').on('keyup', function(e) {
 
   var code = (e.keyCode ? e.keyCode : e.which);
   if (code == 9 ) {
 
-    $('.form-check input').change(function (e) {
+    $('#slideOut .form-check input').change(function (e) {
 
       setReasonActive();
 
     })
 
     function setReasonActive() {
-      $('.form-check input').each(function () {
+      $('#slideOut .form-check input').each(function () {
         if ($(this).prop('checked')) {
 
           $(this).parents('.form-check').css('background' ,'#F1C4B2');
@@ -324,13 +336,13 @@ $('.form-check input').on('keyup', function(e) {
 })
 
 
-$('.form-check input').change(function (e) {
+$('#slideOut .form-check input').change(function (e) {
   setReasonActive();
 
 })
 
 function setReasonActive() {
-  $('.form-check input').each(function () {
+  $('#slideOut .form-check input').each(function () {
     if ($(this).prop('checked')) {
 
       $(this).parents('.form-check').css('background' ,'#F1C4B2');
@@ -421,11 +433,7 @@ $("#theform").validate(
 
   })
 
-
-
-
-
-  $(".form-check").on("keyup", function (e) {
+  $("#slideOut .form-check").on("keyup", function (e) {
 
     var code = (e.keyCode ? e.keyCode : e.which);
     if (code == 9) {
@@ -437,15 +445,127 @@ $("#theform").validate(
 
   })
 
+  //end contact form
 
-  $(".backButton").on("keyup", function (e) {
+  //carousel radio buttons
+
+  $('.carousel-inner .yes').parent().addClass('yesBorder')
+  $('.carousel-inner .no').parent().addClass('noBorder')
+
+  $(".carousel-inner input").on("keyup", function (e) {
+
     var code = (e.keyCode ? e.keyCode : e.which);
     if (code == 9) {
-      $(this).find('.circle').css('outline', 'dashed 3px #4599ff')
+      $('.carousel-inner input').each(function() {
+        $(this).css('opacity', '1')
+        $(this).closest('.form-check-inline').find('label').addClass('noContent')
+      });
+
     }
 
   })
 
-  $(".backButton").on('focusout', function() {
-    $(this).find('.circle').css('outline', 'none')
+  let changeColor = function() {
+
+    var radio = $(this).find('input[type=radio]').prop('checked', true);
+
+    $('input:not(:checked)').parent().removeClass("backgroundColor");
+    $('input:checked').parent().addClass("backgroundColor");
+
+
+  }
+
+  $('.form-check-inline').keypress(
+    changeColor
+
+  ).click(
+    changeColor
+  );
+
+
+  //show radio buttons
+
+
+  let toggle = function() {
+
+    $(this).addClass('toggle')
+
+    $('.carousel').slideDown()
+
+    $('.questionBox .hidden').show().css('display', 'flex')
+    $('.questionBox .content').hide()
+  }
+
+
+  $('.questionBox').keypress(
+    toggle
+
+  ).click(
+    toggle
+  );
+
+
+  let closeToggle = function(e) {
+    e.stopPropagation()
+    $('.questionBox').removeClass('toggle')
+
+    $('.carousel').slideUp()
+
+    $('.questionBox .hidden').hide()
+
+    $('.questionBox .content').fadeIn()
+
+  }
+
+
+  $('.closeRadio').keypress(
+    closeToggle
+
+  ).click(
+    closeToggle
+  );
+
+
+
+  //hide show the carousel arrows
+
+  $('.carousel-control-prev').hide();
+
+  $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
+
+    var slidingItemsAsIndex = $('.carousel-item').length - 1;
+
+    if($(e.relatedTarget).index() == slidingItemsAsIndex ){
+      $('.carousel-control-next').hide();
+    }
+    else{
+      $('.carousel-control-next').show();
+    }
+
+    if($(e.relatedTarget).index() == 0){
+      $('.carousel-control-prev').hide();
+    }
+    else{
+      $('.carousel-control-prev').show();
+    }
+
   })
+
+
+  //special cursor on mobile
+
+
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30;
+  });
+
+  setInterval(function(){
+    xp += ((mouseX - xp)/6);
+    yp += ((mouseY - yp)/6);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
