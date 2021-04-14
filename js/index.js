8d0394cc8fd0304405f094d643fc7f23470b33ac
Elizabeth <elizabeth@Elizabeths-iMac.local>
2021-04-14 07:55:26 -0400

    more bullshit

--- js/index.js
+++ js/index.js
@@ -966,6 +966,59 @@ $('.form-check-input').on('focusout', function() {
 })
 
 
+//IE message
+
+var version = detectIE();
+
+if (version === false) {} else if (version >= 12) {} else {
+  $('#myModal2').modal('show');
+}
+
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  return false;
+}
+
+
+
+//IE message end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //sort
 
 
